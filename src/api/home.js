3e5fa11ea15ba39055036111aec9a37cde8c9da3
Dheeraj Gupta <dj13042002@gmail.com>
2024-07-18 00:35:07 +0530

    changed api url

--- src/api/home.js
+++ src/api/home.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseURL = "https://portfolio-back-8f34qzs3z-dheeraj13042002.vercel.app"
-const baseURL = "http://portfolio-back-end-three.vercel.app"; // for localhost
+const baseURL = "https://portfolio-back-end-lmhm-pzrxpeut0-dheeraj13042002s-projects.vercel.app"; // for localhost
 // const baseURL = "http://localhost:8080"
 
 export const getHomeData =  async () => {
